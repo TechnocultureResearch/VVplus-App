ecf60e7a99d43c94fa862aacf2fcefdfd189baed
krishna40-dot <krish.nand.i40@gmail.com>
2022-04-29 18:11:54 +0530

    ci:creted stage dropdown_bloc,and stage_repository

---
+++ lib/Application/Bloc/Dropdown_Bloc/stage_dropdown_bloc.dart
@@ -0,0 +1,21 @@
+
+import 'package:find_dropdown/rxdart/behavior_subject.dart';
+import 'package:vvplus_app/infrastructure/Models/stage_model.dart';
+import 'package:vvplus_app/infrastructure/Repository/stage_repository.dart';
+
+class StageDropdownBloc{
+ final stageDropdownRepository = StageRepository();
+ final stageDropdownGetData = BehaviorSubject<Stage>();
+
+ Future<List<Stage>> stageDropdownData;
+ Stream<Stage> get selectedStageState => stageDropdownGetData;
+ void selectedStageStateEvent(Stage state) => stageDropdownGetData.add(state);
+
+ CreditAccountDropdownBloc(){
+   stageDropdownData = stageDropdownRepository.getStageData();
+ }
+
+ void dispose(){
+   stageDropdownGetData.close();
+ }
+}
---
+++ lib/infrastructure/Repository/stage_repository.dart
@@ -0,0 +1,22 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../../data_source/api/api_services.dart';
+import '../Models/stage_model.dart';
+
+class StageRepository{
+Client client = Client();
+  Future<List<Stage>> getStageData() async {
+    try{
+      final response = await client.get(Uri.parse(ApiService.getStagenewURL));
+      final items = (jsonDecode(response.body)as List)
+      .map((e) => Stage.fromJson(e))
+      .toList();
+      return items;
+    }catch(e){
+      rethrow;
+    }
+  }
+}
