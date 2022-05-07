79c75116045be1dfeeb5b28c047a9847c4b096da
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-07 11:22:20 +0530

    ci:In PtoP transfer voucher,fromgodow,issuedto api integrated successfully

--- lib/ui/pages/Staff UI/screens/purchase page/Material_request_entry/material_request_entry_body.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/Material_request_entry/material_request_entry_body.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print, prefer_typing_uninitialized_variables, non_constant_identifier_names, deprecated_member_use
 
 import 'dart:convert';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
