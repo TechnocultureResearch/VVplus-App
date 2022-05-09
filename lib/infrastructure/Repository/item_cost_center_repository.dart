6e74c13ae636eb23074810666f345bd497f64a8e
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-09 11:47:28 +0530

    ci:toCostCenter repo cretaed For PhaseToPhase Transfer

--- lib/infrastructure/Repository/item_cost_center_repository.dart
+++ lib/infrastructure/Repository/item_cost_center_repository.dart
@@ -54,6 +54,16 @@ Future<List<ItemCostCenter>> getPhaseToPhaseFromCostCenterData() async {
       rethrow;
     }
 }
+Future<List<ItemCostCenter>> getPhaseToPhaseToCostCenterData() async {
+    try{
+      final response = await client.get(Uri.parse(ApiService.getPhaseToPhaseToCostCenternewURL));
+      final items = (jsonDecode(response.body)as List)
+      .map((e) => ItemCostCenter.fromJson(e)).toList();
+      return items;
+    }catch(e){
+      rethrow;
+    }
+}
 }
 Future<List<ItemCostCenter>> createUser(String strSubCode,String strName) async{
   final response = await http.post(Uri.parse(ApiService.getItemCostCenterURL), body: {
