5837417203059d5f8f6a3b789a1fdf1b7f06d23d
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-12 13:27:45 +0530

    feat: indent selection Repo created for indent approval

---
+++ lib/infrastructure/Repository/indent_selection_repository.dart
@@ -0,0 +1,25 @@
+
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:vvplus_app/data_source/api/api_services.dart';
+
+import '../Models/indent_selection_model.dart';
+
+class IndentSelectionRepositiry{
+  Client client = Client();
+
+  Future<List<IndentSelection>> getIndentSelectionData() async {
+    try{
+      final response = await client.get(
+          Uri.parse(ApiService.getMaterialReqApprovalIndentSelectionnewURL));
+      final items = (jsonDecode(response.body) as List)
+          .map((e) => IndentSelection.fromJson(e))
+          .toList();
+      return items;
+  }catch(e){
+      rethrow;
+    }
+  }
+}
