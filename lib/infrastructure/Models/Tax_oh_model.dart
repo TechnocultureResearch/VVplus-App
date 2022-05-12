84c75a9e2d8d9ec875b717e78ad3592485c93752
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-12 13:32:19 +0530

    ci: indent selection api url added

--- lib/infrastructure/Models/Tax_oh_model.dart
+++ lib/infrastructure/Models/Tax_oh_model.dart
@@ -1,6 +1,5 @@
 
 
-import 'dart:ffi';
 
 class TAXOH {
   String Code;
@@ -29,6 +28,6 @@ class TAXOH {
     "Name": Name, "Code": Code, "Description": Description, "Account": Account,
     "SubExpense": SubExpense,  "AcName": AcName, "ShortName": ShortName,
     "SubExpCode": SubExpCode, "ExpCode": ExpCode, "R0ff": R0ff,
-    // "RC_TaxPer": RC_TaxPer, "TaxPer": TaxPer,
+    "RC_TaxPer": RC_TaxPer, "TaxPer": TaxPer,
   };
 }
