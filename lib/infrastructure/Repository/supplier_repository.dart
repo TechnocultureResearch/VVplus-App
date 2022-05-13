55aca2fb2043712c11c7631a394c0ca7f7f77796
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-13 16:41:51 +0530

    feat: supplier_ repo cretaed for PO

---
+++ lib/infrastructure/Repository/supplier_repository.dart
@@ -0,0 +1,23 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:vvplus_app/data_source/api/api_services.dart';
+import 'package:vvplus_app/infrastructure/Models/supplier_model.dart';
+
+class SupplierRepository{
+  
+  Client client = Client();
+  Future<List<Supplier>> getSupplierData() async {
+    try {
+      final response = await client.get(
+          Uri.parse(ApiService.getSuppliernewURL));
+      final items = (jsonDecode(response.body) as List)
+          .map((e) => Supplier.fromJson(e))
+          .toList();
+      return items;
+    }catch(e){
+      rethrow;
+    }
+  }
+}
