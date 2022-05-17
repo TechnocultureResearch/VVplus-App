45f564fa279a1f18216085d7b84f5a997d596166
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-17 13:14:28 +0530

    ci: FromItemCostCenter repo created for BTOBsend

--- lib/infrastructure/Repository/item_cost_center_repository.dart
+++ lib/infrastructure/Repository/item_cost_center_repository.dart
@@ -64,6 +64,17 @@ Future<List<ItemCostCenter>> getPhaseToPhaseToCostCenterData() async {
       rethrow;
     }
 }
+Future<List<ItemCostCenter>> getBToBsendFromCostCenterData() async {
+    try{
+      final response = await client.get(Uri.parse(ApiService.getBToBSendFromCostCenter));
+      final items = (jsonDecode(response.body)as List)
+      .map((e) => ItemCostCenter.fromJson(e))
+      .toList();
+      return items;
+    }catch(e){
+      rethrow;
+    }
+}
 }
 Future<List<ItemCostCenter>> createUser(String strSubCode,String strName) async{
   final response = await http.post(Uri.parse(ApiService.getItemCostCenterURL), body: {
