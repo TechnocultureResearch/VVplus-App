3966e7b42266d09364cd07f8c15d47540bf8b8d7
payalrparmar <payalparmar.r1@gmail.com>
2022-05-17 14:24:40 +0530

    perf:add media query in custom tab bar for cheque entry

--- lib/ui/pages/Staff UI/screens/sales page/cheque entry/custom_tab_bar.dart
+++ lib/ui/pages/Staff UI/screens/sales page/cheque entry/custom_tab_bar.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:vvplus_app/ui/widgets/constants/colors.dart';
 
-class CustomTabBar extends StatelessWidget{
+class CustomTabBar extends StatelessWidget {
   const CustomTabBar({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return ClipRRect(
       borderRadius: BorderRadius.circular(30),
       child: Container(
-        width: 343,
-        height: 53,
+        width: width * .9,
+        height: height * .08,
+        // width: 343,
+        // height: 53,
         color: primaryColor8,
         child: TabBar(
           tabs: const [
-            Tab(text: 'Receive',),
+            Tab(
+              text: 'Receive',
+            ),
             Tab(text: 'Deposit'),
             Tab(text: 'Cleared'),
           ],
           labelPadding: const EdgeInsets.symmetric(vertical: 4),
           indicator: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            border: Border.all(color:primaryColor8),
+            border: Border.all(color: primaryColor8),
             color: primaryColor3,
           ),
           labelColor: primaryColor1,
           unselectedLabelColor: primaryColor9,
-          labelStyle: const TextStyle(fontSize:15,fontWeight: FontWeight.bold),
-          unselectedLabelStyle: const TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
+          labelStyle:
+              const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          unselectedLabelStyle:
+              const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
         ),
       ),
     );
   }
-
 }
-
