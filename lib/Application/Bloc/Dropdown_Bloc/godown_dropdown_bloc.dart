403e4db60eaadbe9354c73080b83bdbca455b91a
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-17 13:53:23 +0530

    ci: formGodowndropdown added for BranchToBranchSend

--- lib/Application/Bloc/Dropdown_Bloc/godown_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/godown_dropdown_bloc.dart
@@ -7,10 +7,12 @@ class GodownDropdownBloc{
   final godownDropdownGetdata = BehaviorSubject<Godown>();
   final godownPhaseToPhaseFromGetdata = BehaviorSubject<Godown>();
   final godownPhaseToPhaseToGetData = BehaviorSubject<Godown>();
+  final godownBranchToBranchSendFromGetData = BehaviorSubject<Godown>();
 
   Future<List<Godown>> godownDropDownData;
   Future<List<Godown>> godownPhaseToPhaseFromData;
   Future<List<Godown>> godownPhaseToPhaseToData;
+  Future<List<Godown>> godownBranchToBranchSendFromData;
 
   Stream<Godown> get selectedState => godownDropdownGetdata;
   void selectedStateEvent(Godown state)=> godownDropdownGetdata.add(state);
@@ -21,12 +23,19 @@ class GodownDropdownBloc{
   Stream<Godown> get selectedPhaseToPhaseToState => godownPhaseToPhaseFromGetdata;
   void selectedPhaseToPhaseToStateEvent(Godown state) => godownPhaseToPhaseFromGetdata.add(state);
 
+  Stream<Godown> get selectedBranchToBranchSendFromState => godownBranchToBranchSendFromGetData;
+  void selectedBranchToBranchSendFromStateEvent(Godown state) => godownBranchToBranchSendFromGetData.add(state);
+
   GodownDropdownBloc(){
     godownDropDownData = godownDropdownRepository.getData();
     godownPhaseToPhaseFromData = godownDropdownRepository.getPhaseToPhaseFromGodownData();
     godownPhaseToPhaseToData = godownDropdownRepository.getPhaseToPhaseToGodownData();
+    godownBranchToBranchSendFromData = godownDropdownRepository.getBranchToBranchSendFromGodownData();
   }
   void dispose(){
     godownDropdownGetdata.close();
+    godownPhaseToPhaseFromGetdata.close();
+    godownPhaseToPhaseToGetData.close();
+    godownBranchToBranchSendFromGetData.close();
   }
 }
