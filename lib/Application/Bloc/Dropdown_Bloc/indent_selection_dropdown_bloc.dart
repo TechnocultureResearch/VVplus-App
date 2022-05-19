3fdd6cac2d26224f1023ba7faa0c0ba5ef858a03
payalrparmar <payalparmar.r1@gmail.com>
2022-05-19 18:29:11 +0530

    ci: Indent Selection Bloc created for BranchToBranch send

--- lib/Application/Bloc/Dropdown_Bloc/indent_selection_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/indent_selection_dropdown_bloc.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:find_dropdown/rxdart/behavior_subject.dart';
 import 'package:vvplus_app/infrastructure/Models/indent_selection_model.dart';
 import 'package:vvplus_app/infrastructure/Repository/indent_selection_repository.dart';
@@ -7,15 +5,29 @@ import 'package:vvplus_app/infrastructure/Repository/indent_selection_repository
 class IndentSelectionDropdownBloc {
   final indentSelectionRepository = IndentSelectionRepositiry();
   final indentSelectionDropdownGetData = BehaviorSubject<IndentSelection>();
+  final indentSelectionBtoBGetData = BehaviorSubject<IndentSelection>();
 
   Future<List<IndentSelection>> indentSelectionDropdownData;
-  Stream<IndentSelection> get selectedIndentSelectionState => indentSelectionDropdownGetData;
-  void selectedIndentSelectionStateEvent(IndentSelection state) => indentSelectionDropdownGetData.add(state);
+  Future<List<IndentSelection>> indentSelectionBtoBDropdownData;
+
+  Stream<IndentSelection> get selectedIndentSelectionState =>
+      indentSelectionDropdownGetData;
+  void selectedIndentSelectionStateEvent(IndentSelection state) =>
+      indentSelectionDropdownGetData.add(state);
+
+  Stream<IndentSelection> get selectedIndentSelectionBtoBState =>
+      indentSelectionBtoBGetData;
+  void selectedIndentSelectionBtoBStateEvent(IndentSelection state) =>
+      indentSelectionBtoBGetData.add(state);
 
   IndentSelectionDropdownBloc() {
-    indentSelectionDropdownData = indentSelectionRepository.getIndentSelectionData();
+    indentSelectionDropdownData =
+        indentSelectionRepository.getIndentSelectionData();
+    indentSelectionBtoBDropdownData =
+        indentSelectionRepository.getBranchSendIndentSelectionData();
   }
   void dispose() {
     indentSelectionDropdownGetData.close();
+    indentSelectionBtoBGetData.close();
   }
 }
