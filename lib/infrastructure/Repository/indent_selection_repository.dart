20559aec70cbbc7c52fc63d7f2e2dcfff392250e
payalrparmar <payalparmar.r1@gmail.com>
2022-05-19 18:24:16 +0530

    ci:Indent Selection repo created for Branch to Branch send

--- lib/infrastructure/Repository/indent_selection_repository.dart
+++ lib/infrastructure/Repository/indent_selection_repository.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 import 'package:http/http.dart';
@@ -7,18 +5,31 @@ import 'package:vvplus_app/data_source/api/api_services.dart';
 
 import '../Models/indent_selection_model.dart';
 
-class IndentSelectionRepositiry{
+class IndentSelectionRepositiry {
   Client client = Client();
 
   Future<List<IndentSelection>> getIndentSelectionData() async {
-    try{
+    try {
       final response = await client.get(
           Uri.parse(ApiService.getMaterialReqApprovalIndentSelectionnewURL));
       final items = (jsonDecode(response.body) as List)
           .map((e) => IndentSelection.fromJson(e))
           .toList();
       return items;
-  }catch(e){
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<IndentSelection>> getBranchSendIndentSelectionData() async {
+    try {
+      final response = await client
+          .get(Uri.parse(ApiService.getIndentSelectionBToBSendnewURL));
+      final items = (jsonDecode(response.body) as List)
+          .map((e) => IndentSelection.fromJson(e))
+          .toList();
+      return items;
+    } catch (e) {
       rethrow;
     }
   }
