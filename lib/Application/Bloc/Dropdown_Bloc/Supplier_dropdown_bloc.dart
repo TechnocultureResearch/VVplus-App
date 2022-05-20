0c6b86978cf0b7abf6578cf562baf867b5094f37
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-20 18:31:00 +0530

    ci: supplier Bloc added for BToBreceive

--- lib/Application/Bloc/Dropdown_Bloc/Supplier_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/Supplier_dropdown_bloc.dart
@@ -7,16 +7,23 @@ import 'package:vvplus_app/infrastructure/Repository/supplier_repository.dart';
 class SupplierDropdownBloc{
  final supplierDropdownRepository = SupplierRepository();
  final supplierDropdownGetData = BehaviorSubject<Supplier>();
+ final supplierBToBReceiveDropdownGetData = BehaviorSubject<Supplier>();
 
  Future<List<Supplier>> supplierDropdownData;
+ Future<List<Supplier>> supplierBToBReceiveDropdownData;
 
  Stream<Supplier> get selectedSupplierState => supplierDropdownGetData;
  void selectedSupplierStateEvent(Supplier state) => supplierDropdownGetData.add(state);
 
+ Stream<Supplier> get selectedSupplierBToBReceiveState => supplierBToBReceiveDropdownGetData;
+ void selectedSupplierBToBReceiveStateEvent(Supplier state) => supplierBToBReceiveDropdownGetData.add(state);
+
 SupplierDropdownBloc(){
   supplierDropdownData = supplierDropdownRepository.getSupplierData();
+  supplierBToBReceiveDropdownData = supplierDropdownRepository.getSupplierBToBReceiveData();
 }
 void dispose(){
   supplierDropdownGetData.close();
+  supplierBToBReceiveDropdownGetData.close();
 }
 }
