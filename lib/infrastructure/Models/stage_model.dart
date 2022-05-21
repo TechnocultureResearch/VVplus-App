3163aeeba24a5ae67429402fd6b3aa86259b57a7
payalrparmar <payalparmar.r1@gmail.com>
2022-05-21 17:16:59 +0530

    perf: Enable search keyword as String

--- lib/infrastructure/Models/stage_model.dart
+++ lib/infrastructure/Models/stage_model.dart
@@ -1,5 +1,4 @@
-
-class Stage{
+class Stage {
   String SearchCode;
   String Name;
   String Date;
@@ -10,19 +9,20 @@ class Stage{
     this.Name,
     this.Date,
     this.DueDate,
-});
+  });
+  @override
+  String toString() => SearchCode;
 
   factory Stage.fromJson(Map<String, dynamic> json) => Stage(
-    SearchCode: json["SearchCode"],
-    Name: json["Name"],
-    Date: json["Date"],
-    DueDate: json["DueDate"]
-  );
+      SearchCode: json["SearchCode"],
+      Name: json["Name"],
+      Date: json["Date"],
+      DueDate: json["DueDate"]);
 
   Map<String, dynamic> toJson() => {
-    "SearchCode": SearchCode,
-    "Name": Name,
-    "Date": Date,
-    "DueDate": DueDate
-  };
+        "SearchCode": SearchCode,
+        "Name": Name,
+        "Date": Date,
+        "DueDate": DueDate
+      };
 }
