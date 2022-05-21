93fd372fec46a173c9d9edc6bb4eb8405d45fcf6
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-21 18:51:45 +0530

    ci: FillTransfer newUrl added for BToB Receive

--- lib/data_source/api/api_services.dart
+++ lib/data_source/api/api_services.dart
@@ -1,5 +1,8 @@
 // get and post urls
+import 'package:vvplus_app/infrastructure/Models/supplier_model.dart';
+
 class ApiService {
+
   static const String postApiURL =
       "http://103.136.82.200:777/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FPostIndent?StrRecord=${'{"StrIndTypeCode":"IND","StrSiteCode":"AD","StrIndNo":"11","StrIndDate":"10/11/2021","StrDepartmentCode":"AD2","StrIndentorCode":"SG344","StrPreparedByCode":"SA","StrIndGrid":[{"StrItemCode":"PN1","DblQuantity":"100","StrCostCenterCode":"AD1","StrRequiredDate":"10/11/2021","StrRemark":"remark2"}]}'}";
 
@@ -152,6 +155,7 @@ class ApiService {
 
   static const String getGodownBToBReceivenewUrl = "http://43.228.113.108:888/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FGetStkIntransit?StrRecord=${'{"StrFilter":"Godown","StrSiteCode":"AA","StrPartyCode":"","StrStkTrnManID":""}'}";
 
+  static const String getFillTransfernewURL = "http://43.228.113.108:888/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FGetStkIntransit?StrRecord=${'{"StrFilter":"FillTransfer","StrSiteCode":"AA","StrPartyCode":"SG614","StrStkTrnManID":""}'}";
   // mock database get url
 
   static const String mockDataIndentorNameURL =
