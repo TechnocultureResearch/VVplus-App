5a3201c6b87793f5caba8b803f1a20f7a0900880
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-26 16:43:59 +0530

    ci: indentNo DropdownBloc Added for Material Requet Approv....

--- lib/Application/Bloc/Dropdown_Bloc/indent_no_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/indent_no_dropdown_bloc.dart
@@ -7,15 +7,23 @@ import 'package:vvplus_app/infrastructure/Repository/indent_no_repository.dart';
 class IndentNoDropdownBloc{
  final indentNoDropdownRepository = IndentNoRepository();
  final indentNoDropdownGetData = BehaviorSubject<IndentNo>();
+final indentNoMaterialRequestApprovDropdownGetData = BehaviorSubject<IndentNo>();
 
  Future<List<IndentNo>> indentNoDropdownData;
+ Future<List<IndentNo>> indentNoMaterialRequestApproveDropdowndata;
+
  Stream<IndentNo> get selectedIndentNoState => indentNoDropdownGetData;
  void selectedIndentNoStateEvent(IndentNo state) => indentNoDropdownGetData.add(state);
 
+ Stream<IndentNo> get selectedIndentNoMaterialReqApprovState => indentNoMaterialRequestApprovDropdownGetData;
+ void selectedIndentNoMaterialReqApprovStateEvent(IndentNo state) => indentNoMaterialRequestApprovDropdownGetData.add(state);
+
  IndentNoDropdownBloc(){
    indentNoDropdownData = indentNoDropdownRepository.getIndentNoBToBSendData();
+   indentNoMaterialRequestApproveDropdowndata = indentNoDropdownRepository.getIndentNoMaterialReqApprovData();
  }
  void dispose(){
    indentNoDropdownGetData.close();
+   indentNoMaterialRequestApprovDropdownGetData.close();
  }
 }
