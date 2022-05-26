8abb3dc6cef6d1132a9d42ebcb9506ab16d15e59
payalrparmar <payalparmar.r1@gmail.com>
2022-05-26 18:23:02 +0530

    ci: Supplier repo added for Good receipt entry

--- lib/infrastructure/Repository/supplier_repository.dart
+++ lib/infrastructure/Repository/supplier_repository.dart
@@ -1,34 +1,47 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:vvplus_app/data_source/api/api_services.dart';
 import 'package:vvplus_app/infrastructure/Models/supplier_model.dart';
 
-class SupplierRepository{
-  
+class SupplierRepository {
   Client client = Client();
   Future<List<Supplier>> getSupplierData() async {
     try {
-      final response = await client.get(
-          Uri.parse(ApiService.getSuppliernewURL));
+      final response =
+          await client.get(Uri.parse(ApiService.getSuppliernewURL));
       final items = (jsonDecode(response.body) as List)
           .map((e) => Supplier.fromJson(e))
           .toList();
       return items;
-    }catch(e){
+    } catch (e) {
       rethrow;
     }
   }
 
   Future<List<Supplier>> getSupplierBToBReceiveData() async {
-    try{
-      final response = await client.get(Uri.parse(ApiService.getSupplierBToBReceivenewURL));
-      final items = (jsonDecode(response.body)as List)
-      .map((e) => Supplier.fromJson(e))
-      .toList();
+    try {
+      final response =
+          await client.get(Uri.parse(ApiService.getSupplierBToBReceivenewURL));
+      final items = (jsonDecode(response.body) as List)
+          .map((e) => Supplier.fromJson(e))
+          .toList();
       return items;
-    }catch(e){
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<Supplier>> getSupplierGoodReceiptData() async {
+    try {
+      final response =
+          await client.get(Uri.parse(ApiService.getSupplierGoodReceiptnewURL));
+      final items = (jsonDecode(response.body) as List)
+          .map((e) => Supplier.fromJson(e))
+          .toList();
+      return items;
+    } catch (e) {
+      rethrow;
     }
   }
 }
