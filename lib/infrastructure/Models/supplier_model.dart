54db76fab5f1713afcc0a10bf4553acaacac5e3a
payalrparmar <payalparmar.r1@gmail.com>
2022-05-26 18:21:36 +0530

    ci: supplier data added in Supplier Model

--- lib/infrastructure/Models/supplier_model.dart
+++ lib/infrastructure/Models/supplier_model.dart
@@ -9,6 +9,11 @@ class Supplier {
   String Form_Code;
   String Form_Desc;
 
+  String subCode;
+  String name;
+  String cityName;
+  String manualCode;
+
   Supplier(
       {this.SubCode,
       this.Name,
@@ -18,7 +23,11 @@ class Supplier {
       this.Form_Code,
       this.GSTIN,
       this.Location,
-      this.Form_Desc});
+      this.Form_Desc,
+      this.subCode,
+      this.name,
+      this.cityName,
+      this.manualCode});
   @override
   String toString() => Name;
   factory Supplier.fromJson(Map<String, dynamic> json) => Supplier(
@@ -30,7 +39,11 @@ class Supplier {
       State: json["State"],
       Form_Desc: json["Form_Desc"],
       GSTIN: json["GSTIN"],
-      Location: json["Location"]);
+      Location: json["Location"],
+      subCode: json["SubCode"],
+      name: json["Name"],
+      cityName: json["CityName"],
+      manualCode: json["ManualCode"]);
 
   Map<String, dynamic> toJson() => {
         "Name": Name,
@@ -42,5 +55,9 @@ class Supplier {
         "Form_Desc": Form_Desc,
         "GSTIN": GSTIN,
         "Location": Location,
+        "SubCode": subCode,
+        "Name": name,
+        "CityName": cityName,
+        "ManualCode": manualCode,
       };
 }
