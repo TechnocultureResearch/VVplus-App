e383ee45e808d983d0b5c71ee7cf7028387bfee1
payalrparmar <payalparmar.r1@gmail.com>
2022-05-26 18:24:05 +0530

    ci: supplier bloc added for Good receipt entry

--- lib/Application/Bloc/Dropdown_Bloc/Supplier_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/Supplier_dropdown_bloc.dart
@@ -1,29 +1,41 @@
-
-
 import 'package:find_dropdown/rxdart/behavior_subject.dart';
 import 'package:vvplus_app/infrastructure/Models/supplier_model.dart';
 import 'package:vvplus_app/infrastructure/Repository/supplier_repository.dart';
 
-class SupplierDropdownBloc{
- final supplierDropdownRepository = SupplierRepository();
- final supplierDropdownGetData = BehaviorSubject<Supplier>();
- final supplierBToBReceiveDropdownGetData = BehaviorSubject<Supplier>();
+class SupplierDropdownBloc {
+  final supplierDropdownRepository = SupplierRepository();
+  final supplierDropdownGetData = BehaviorSubject<Supplier>();
+  final supplierBToBReceiveDropdownGetData = BehaviorSubject<Supplier>();
+  final supplierGoodReceiptDropdownGetData = BehaviorSubject<Supplier>();
 
- Future<List<Supplier>> supplierDropdownData;
- Future<List<Supplier>> supplierBToBReceiveDropdownData;
+  Future<List<Supplier>> supplierDropdownData;
+  Future<List<Supplier>> supplierBToBReceiveDropdownData;
+  Future<List<Supplier>> supplierGoodReceiptDropdownData;
 
- Stream<Supplier> get selectedSupplierState => supplierDropdownGetData;
- void selectedSupplierStateEvent(Supplier state) => supplierDropdownGetData.add(state);
+  Stream<Supplier> get selectedSupplierState => supplierDropdownGetData;
+  void selectedSupplierStateEvent(Supplier state) =>
+      supplierDropdownGetData.add(state);
 
- Stream<Supplier> get selectedSupplierBToBReceiveState => supplierBToBReceiveDropdownGetData;
- void selectedSupplierBToBReceiveStateEvent(Supplier state) => supplierBToBReceiveDropdownGetData.add(state);
+  Stream<Supplier> get selectedSupplierBToBReceiveState =>
+      supplierBToBReceiveDropdownGetData;
+  void selectedSupplierBToBReceiveStateEvent(Supplier state) =>
+      supplierBToBReceiveDropdownGetData.add(state);
 
-SupplierDropdownBloc(){
-  supplierDropdownData = supplierDropdownRepository.getSupplierData();
-  supplierBToBReceiveDropdownData = supplierDropdownRepository.getSupplierBToBReceiveData();
-}
-void dispose(){
-  supplierDropdownGetData.close();
-  supplierBToBReceiveDropdownGetData.close();
-}
+  Stream<Supplier> get selectedSupplierGoodReceiptState =>
+      supplierGoodReceiptDropdownGetData;
+  void selectedSupplierGoodReceiptStateEvent(Supplier state) =>
+      supplierGoodReceiptDropdownGetData.add(state);
+
+  SupplierDropdownBloc() {
+    supplierDropdownData = supplierDropdownRepository.getSupplierData();
+    supplierBToBReceiveDropdownData =
+        supplierDropdownRepository.getSupplierBToBReceiveData();
+    supplierGoodReceiptDropdownData =
+        supplierDropdownRepository.getSupplierGoodReceiptData();
+  }
+  void dispose() {
+    supplierDropdownGetData.close();
+    supplierBToBReceiveDropdownGetData.close();
+    supplierGoodReceiptDropdownGetData.close();
+  }
 }
