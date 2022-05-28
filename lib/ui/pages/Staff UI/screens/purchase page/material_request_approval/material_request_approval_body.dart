98cf4c2bab10a954deb7d5465e322a55c30e975c
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-28 19:17:58 +0530

    feat: added design and integrated items api

--- lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 import 'package:search_choices/search_choices.dart';
 import 'package:vvplus_app/Application/Bloc/Dropdown_Bloc/indent_no_dropdown_bloc.dart';
@@ -11,8 +15,12 @@ import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/form_text.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_containers.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/text_form_field.dart';
 import 'package:vvplus_app/ui/widgets/constants/size.dart';
+import 'package:http/http.dart' as http;
 
+import '../../../../../../domain/common/snackbar_widget.dart';
 import '../../../../../../infrastructure/Models/indent_no_model.dart';
+import '../../../../../widgets/Utilities/rounded_button.dart';
+import '../../../../../widgets/constants/colors.dart';
 
 class MaterialRequestApprovalBody extends StatefulWidget{
   const MaterialRequestApprovalBody({Key key}) : super(key: key);
@@ -27,18 +35,44 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
   IndentorNameDropdownBloc _dropdownBloc;
   IndentNoDropdownBloc indentNoDropdownBloc;
   bool pressAttention = false;
+  Future<List<dynamic>> futureList;
+  List<String> itemList = [];
+
   @override
   void initState() {
-
+    futureList =  getIndentItemData();
     dateinput.text = "";
-    _dropdownBloc = IndentorNameDropdownBloc();
+    // _dropdownBloc = IndentorNameDropdownBloc();
     indentNoDropdownBloc = IndentNoDropdownBloc();
     super.initState();
   }
-  IndentorName selectIndentorName;
+
+
+  Future<List<dynamic>> getIndentItemData() async{
+    if(selectIndentNo!= null){
+      try{
+        var url = Uri.parse('http://43.228.113.108:888/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FGetIndentApproval?StrRecord=${'{"StrFilter":"FillGrid","StrSiteCode":"AD","StrV_Date":"2022-05-27",StrIndDocID:[{"StrDocID":""},{"StrDocID":""}]}'}');
+        final response = await http.get(url);
+        final List<dynamic> items = json.decode(response.body);
+        // final items = (jsonDecode(response.body) as List)
+        // .map((e) => IndentorName.fromJson(e))
+        //     .toList();
+        print('Response Status: ${response.statusCode}');
+        print('Response Body: $items');
+        return items ;
+      }catch(e){
+        rethrow;
+      }
+    }
+    // else{
+    //    print("Please select indent No");
+    // }
+  }
+  // IndentorName selectIndentorName;
   IndentNo selectIndentNo;
   void onDataChange1(IndentNo state) {
     setState(() {
+      futureList = getIndentItemData();
       selectIndentNo = state;
     });
   }
@@ -62,7 +96,7 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+            children:[
               sizedbox1,
               formsHeadText("Indant Date"),
               Container(
@@ -130,12 +164,55 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
                   ),
                 ),
               ),
+              SizedBox(height: 10,),
+              StreamBuilder(
+                stream: getIndentItemData().asStream(),
+    builder: (context, snapshot){
+    if(snapshot.hasData){
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 15.0),
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: snapshot.data.length,
+          itemBuilder:(context, index){
+          return Card(
+child: ListTile(
+  leading: Text(snapshot.data[index]['Item']),
+),
+          );
+          }
+        ),
+      );
+//         return Column(
+//           children:<Widget>[
+//               ListView.builder(
+//                 shrinkWrap: true,
+//                  itemExtent: snapshot.data.length,
+//       itemBuilder: (context, index) =>
+//                 SingleChildScrollView( scrollDirection: Axis.vertical,
+//                   child: Card(elevation:5.0,margin:  EdgeInsets.symmetric(vertical: 1,horizontal: 6),
+//   child: ListTile(
+//   leading: Text(snapshot.data[index]['Item']),
+//
+// ),
+//                   ),
+//                 )
+//
+//                ),
+//           ]
+//         );
+    }
+    else{
+      return
+      Center(child: CircularProgressIndicator());}
+    }
+              ),
+              // const InformationBoxContainer1(),
               //SizedBox(height: 10,),
               //const InformationBoxContainer1(),
-              //SizedBox(height: 10,),
-              //const InformationBoxContainer1(),
-              const MaterialApprovalPageContainerData(),
-            /*  sizedbox1,
+              // const MaterialApprovalPageContainerData(),
+              sizedbox1,
               Padding(
                   padding: padding4,
                   child: roundedButtonHome2("Approve",(){
@@ -146,7 +223,7 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
                   child: roundedButtonHome2("Deny",(){},roundedButtonHomeColor2)),
               Padding(
                   padding: padding4,
-                  child: roundedButtonHome2("Wait",(){},roundedButtonHomeColor3)),*/
+                  child: roundedButtonHome2("Wait",(){},roundedButtonHomeColor3)),
             ],
           ),
         ),
