b098f0bb8495ae010404d86055caaed1b7f58d2e
payalrparmar <payalparmar.r1@gmail.com>
2022-05-30 17:42:17 +0530

    Update material_request_approval_body.dart

--- lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
@@ -13,7 +13,9 @@ import 'package:vvplus_app/infrastructure/Models/indentor_name_model.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/screens/purchase%20page/material_request_approval/forms_container_data.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/form_text.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_containers.dart';
+import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_text_style.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/text_form_field.dart';
+import 'package:vvplus_app/ui/widgets/constants/assets.dart';
 import 'package:vvplus_app/ui/widgets/constants/size.dart';
 import 'package:http/http.dart' as http;
 
@@ -22,11 +24,13 @@ import '../../../../../../infrastructure/Models/indent_no_model.dart';
 import '../../../../../widgets/Utilities/rounded_button.dart';
 import '../../../../../widgets/constants/colors.dart';
 
-class MaterialRequestApprovalBody extends StatefulWidget{
+class MaterialRequestApprovalBody extends StatefulWidget {
   const MaterialRequestApprovalBody({Key key}) : super(key: key);
   @override
-  State<MaterialRequestApprovalBody> createState() => MyMaterialRequestApprovalBody();
+  State<MaterialRequestApprovalBody> createState() =>
+      MyMaterialRequestApprovalBody();
 }
+
 class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
   bool isActive = false;
   bool pressed = false;
@@ -40,18 +44,18 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
 
   @override
   void initState() {
-    futureList =  getIndentItemData();
+    futureList = getIndentItemData();
     dateinput.text = "";
     // _dropdownBloc = IndentorNameDropdownBloc();
     indentNoDropdownBloc = IndentNoDropdownBloc();
     super.initState();
   }
 
-
-  Future<List<dynamic>> getIndentItemData() async{
-    if(selectIndentNo!= null){
-      try{
-        var url = Uri.parse('http://43.228.113.108:888/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FGetIndentApproval?StrRecord=${'{"StrFilter":"FillGrid","StrSiteCode":"AD","StrV_Date":"2022-05-27",StrIndDocID:[{"StrDocID":""},{"StrDocID":""}]}'}');
+  Future<List<dynamic>> getIndentItemData() async {
+    if (selectIndentNo != null) {
+      try {
+        var url = Uri.parse(
+            'http://43.228.113.108:888/Individual_WebSite/LoginInfo_WS/WCF/WebService_Test.asmx/FGetIndentApproval?StrRecord=${'{"StrFilter":"FillGrid","StrSiteCode":"AD","StrV_Date":"2022-05-27",StrIndDocID:[{"StrDocID":""},{"StrDocID":""}]}'}');
         final response = await http.get(url);
         final List<dynamic> items = json.decode(response.body);
         // final items = (jsonDecode(response.body) as List)
@@ -59,8 +63,8 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
         //     .toList();
         print('Response Status: ${response.statusCode}');
         print('Response Body: $items');
-        return items ;
-      }catch(e){
+        return items;
+      } catch (e) {
         rethrow;
       }
     }
@@ -68,6 +72,7 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
     //    print("Please select indent No");
     // }
   }
+
   // IndentorName selectIndentorName;
   IndentNo selectIndentNo;
   void onDataChange1(IndentNo state) {
@@ -76,12 +81,13 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
       selectIndentNo = state;
     });
   }
-  Future<void> _refresh() async{
-    await Future.delayed(const Duration(milliseconds: 800),() {
-      setState(() {
-      });
+
+  Future<void> _refresh() async {
+    await Future.delayed(const Duration(milliseconds: 800), () {
+      setState(() {});
     });
   }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -99,18 +105,18 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
+              children: [
                 sizedbox1,
-                formsHeadText("Indant Date"),
+                formsHeadText("Indent Date"),
                 Container(
                   padding: dateFieldPadding,
-                  height: dateFieldHeight,
+                  height: height * .09,
                   child: TextFormField(
-                    validator: (val){
-                      if(val.isEmpty) {
+                    validator: (val) {
+                      if (val.isEmpty) {
                         return 'Enter Detail';
                       }
-                      if(val != dateinput.text) {
+                      if (val != dateinput.text) {
                         return 'Enter Correct Detail';
                       }
                       return null;
@@ -120,94 +126,227 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
                     readOnly: true,
                     onTap: () async {
                       DateTime pickedDate = await showDatePicker(
-                          context: context, initialDate: DateTime.now(),
+                          context: context,
+                          initialDate: DateTime.now(),
                           firstDate: DateTime(2000),
-                          lastDate: DateTime(2101)
-                      );
+                          lastDate: DateTime(2101));
                       if (pickedDate != null) {
-                        String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+                        String formattedDate =
+                            DateFormat('dd-MM-yyyy').format(pickedDate);
                         setState(() {
                           dateinput.text = formattedDate;
                         });
-                      } else {
-                      }
+                      } else {}
                     },
                   ),
                 ),
-                formsHeadText("Indant No"),
+                formsHeadText("Indent No"),
                 Padding(
                   padding: padding1,
                   child: Container(
                     decoration: decorationForms(),
                     child: FutureBuilder<List<IndentNo>>(
-                        future: indentNoDropdownBloc.indentNoMaterialRequestApproveDropdowndata,
+                        future: indentNoDropdownBloc
+                            .indentNoMaterialRequestApproveDropdowndata,
                         builder: (context, snapshot) {
                           return StreamBuilder<IndentNo>(
-                              stream: indentNoDropdownBloc.selectedIndentNoMaterialReqApprovState,
+                              stream: indentNoDropdownBloc
+                                  .selectedIndentNoMaterialReqApprovState,
                               builder: (context, item) {
                                 return SearchChoices<IndentNo>.single(
-                                  icon: const Icon(Icons.keyboard_arrow_down_sharp,size:30),
-                                  padding: selectIndentNo!=null ? 2 : 11,
+                                  icon: const Icon(
+                                      Icons.keyboard_arrow_down_sharp,
+                                      size: 30),
+                                  padding: selectIndentNo != null
+                                      ? height * .002
+                                      : height * .015,
                                   isExpanded: true,
                                   hint: "Search here",
                                   value: selectIndentNo,
                                   displayClearIcon: false,
                                   onChanged: onDataChange1,
                                   items: snapshot?.data
-                                      ?.map<DropdownMenuItem<IndentNo>>((e) {
-                                    return DropdownMenuItem<IndentNo>(
-                                      value: e,
-                                      child: Text(e.StrDocID ?? ''),
-                                    );
-                                  })?.toList() ??[],
+                                          ?.map<DropdownMenuItem<IndentNo>>(
+                                              (e) {
+                                        return DropdownMenuItem<IndentNo>(
+                                          value: e,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(4.0),
+                                            child: Text(e.StrDocID ?? ''),
+                                          ),
+                                        );
+                                      })?.toList() ??
+                                      [],
                                 );
-                              }
-                          );
-                        }
-                    ),
+                              });
+                        }),
                   ),
                 ),
-                SizedBox(height: 10,),
-                StreamBuilder(
-                  stream: getIndentItemData().asStream(),
-      builder: (context, snapshot){
-      if(snapshot.hasData){
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 15.0),
-          child: Container(height: height * 0.35,
-            child: ListView.builder(
-              physics: AlwaysScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: snapshot.data.length,
-              itemBuilder:(context, index){
-              return Card(
-child: ListTile(
-  leading: Text(snapshot.data[index]['Item']),
-),
-              );
-              }
-            ),
-          ),
-        );
-      }
-      else{
-        return
-        Center(child: CircularProgressIndicator());}
-      }
+                SizedBox(
+                  height: 10,
                 ),
+                StreamBuilder(
+                    stream: getIndentItemData().asStream(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 0.0),
+                          child: Container(
+                            height: height * 0.37,
+                            child: ListView.builder(
+                                physics: AlwaysScrollableScrollPhysics(),
+                                scrollDirection: Axis.vertical,
+                                shrinkWrap: true,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      height: height * .15,
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        color: primaryColor3,
+                                        boxShadow: const [
+                                          BoxShadow(
+                                            color: primaryColor4,
+                                            offset: Offset(0.0, 2.0), //(x,y)
+                                            blurRadius: 6.0,
+                                          ),
+                                        ],
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const SizedBox(
+                                                height: 15,
+                                              ),
+                                              Container(
+                                                height: height * .08,
+                                                width: width * .3,
+                                                //color: Colors.green,
+                                                child: Text(
+                                                    snapshot.data[index]
+                                                        ['Item'],
+                                                    maxLines: 2,
+                                                    style:
+                                                        containerTextStyle1()),
+                                              ),
+                                              const SizedBox(
+                                                height: 8,
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    "Order Qty: " +
+                                                        snapshot.data[index]
+                                                            ['indqty'],
+                                                    style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 12),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 8,
+                                                  ),
+                                                  Text(
+                                                    "pc",
+                                                    style:
+                                                        containerTextStyle2(),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                      "Dept: " +
+                                                          snapshot.data[index]
+                                                              ['Dept'],
+                                                      style: TextStyle(
+                                                          color: Colors.green,
+                                                          fontSize: 13)),
+                                                  const SizedBox(
+                                                    width: 80,
+                                                  ),
+                                                  Image.asset(icon15),
+                                                ],
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Text(
+                                                "Req Date: " +
+                                                    snapshot.data[index]
+                                                        ['inddt'],
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 12),
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Container(
+                                                width: width * .44,
+                                                child: Text(
+                                                  "Indentor: " +
+                                                      snapshot.data[index]
+                                                          ['Indentor'],
+                                                  maxLines: 2,
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 12),
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Text(
+                                                "Indent No: " +
+                                                    snapshot.data[index]
+                                                        ['IndSno'],
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 12),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }),
+                          ),
+                        );
+                      } else {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    }),
                 // const InformationBoxContainer1(),
                 //SizedBox(height: 10,),
                 //const InformationBoxContainer1(),
                 // const MaterialApprovalPageContainerData(),
-                sizedbox1,
+                //sizedbox1,
                 Padding(
                     padding: padding4,
-                    child: roundedButtonHome2("Approve",(){
-                    },roundedButtonHomeColor1)),
+                    child: roundedButtonHome2(
+                        "Approve", () {}, roundedButtonHomeColor1)),
                 Padding(
                     padding: padding4,
-                    child: roundedButtonHome2("Deny",(){},roundedButtonHomeColor2)),
+                    child: roundedButtonHome2(
+                        "Deny", () {}, roundedButtonHomeColor2)),
               ],
             ),
           ),
