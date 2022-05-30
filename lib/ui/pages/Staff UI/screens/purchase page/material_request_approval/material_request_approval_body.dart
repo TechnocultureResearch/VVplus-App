42982a6f57f44b0ee6af87ae09daf54adbfe0132
krishna40-dot <krish.nand.i40@gmail.com>
2022-05-30 13:15:41 +0530

    feat: modify design remove wait btn

--- lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/material_request_approval_body.dart
@@ -84,147 +84,132 @@ class MyMaterialRequestApprovalBody extends State<MaterialRequestApprovalBody> {
   }
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      triggerMode: RefreshIndicatorTriggerMode.onEdge,
-      edgeOffset: 20,
-      displacement: 200,
-      strokeWidth: 5,
-      onRefresh: _refresh,
-      child: SingleChildScrollView(
-        child: Form(
-          key: materialRequestApprovalFormKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:[
-              sizedbox1,
-              formsHeadText("Indant Date"),
-              Container(
-                padding: dateFieldPadding,
-                height: dateFieldHeight,
-                child: TextFormField(
-                  validator: (val){
-                    if(val.isEmpty) {
-                      return 'Enter Detail';
-                    }
-                    if(val != dateinput.text) {
-                      return 'Enter Correct Detail';
-                    }
-                    return null;
-                  },
-                  controller: dateinput,
-                  decoration: dateFieldDecoration(),
-                  readOnly: true,
-                  onTap: () async {
-                    DateTime pickedDate = await showDatePicker(
-                        context: context, initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2101)
-                    );
-                    if (pickedDate != null) {
-                      String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
-                      setState(() {
-                        dateinput.text = formattedDate;
-                      });
-                    } else {
-                    }
-                  },
-                ),
-              ),
-              formsHeadText("Indant No"),
-              Padding(
-                padding: padding1,
-                child: Container(
-                  decoration: decorationForms(),
-                  child: FutureBuilder<List<IndentNo>>(
-                      future: indentNoDropdownBloc.indentNoMaterialRequestApproveDropdowndata,
-                      builder: (context, snapshot) {
-                        return StreamBuilder<IndentNo>(
-                            stream: indentNoDropdownBloc.selectedIndentNoMaterialReqApprovState,
-                            builder: (context, item) {
-                              return SearchChoices<IndentNo>.single(
-                                icon: const Icon(Icons.keyboard_arrow_down_sharp,size:30),
-                                padding: selectIndentNo!=null ? 2 : 11,
-                                isExpanded: true,
-                                hint: "Search here",
-                                value: selectIndentNo,
-                                displayClearIcon: false,
-                                onChanged: onDataChange1,
-                                items: snapshot?.data
-                                    ?.map<DropdownMenuItem<IndentNo>>((e) {
-                                  return DropdownMenuItem<IndentNo>(
-                                    value: e,
-                                    child: Text(e.StrDocID ?? ''),
-                                  );
-                                })?.toList() ??[],
-                              );
-                            }
-                        );
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: RefreshIndicator(
+        triggerMode: RefreshIndicatorTriggerMode.onEdge,
+        edgeOffset: 20,
+        displacement: 200,
+        strokeWidth: 5,
+        onRefresh: _refresh,
+        child: SingleChildScrollView(
+          child: Form(
+            key: materialRequestApprovalFormKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+                sizedbox1,
+                formsHeadText("Indant Date"),
+                Container(
+                  padding: dateFieldPadding,
+                  height: dateFieldHeight,
+                  child: TextFormField(
+                    validator: (val){
+                      if(val.isEmpty) {
+                        return 'Enter Detail';
                       }
+                      if(val != dateinput.text) {
+                        return 'Enter Correct Detail';
+                      }
+                      return null;
+                    },
+                    controller: dateinput,
+                    decoration: dateFieldDecoration(),
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime pickedDate = await showDatePicker(
+                          context: context, initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2101)
+                      );
+                      if (pickedDate != null) {
+                        String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+                        setState(() {
+                          dateinput.text = formattedDate;
+                        });
+                      } else {
+                      }
+                    },
                   ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              StreamBuilder(
-                stream: getIndentItemData().asStream(),
-    builder: (context, snapshot){
-    if(snapshot.hasData){
-      return Padding(
-        padding: const EdgeInsets.only(bottom: 15.0),
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          itemCount: snapshot.data.length,
-          itemBuilder:(context, index){
-          return Card(
+                formsHeadText("Indant No"),
+                Padding(
+                  padding: padding1,
+                  child: Container(
+                    decoration: decorationForms(),
+                    child: FutureBuilder<List<IndentNo>>(
+                        future: indentNoDropdownBloc.indentNoMaterialRequestApproveDropdowndata,
+                        builder: (context, snapshot) {
+                          return StreamBuilder<IndentNo>(
+                              stream: indentNoDropdownBloc.selectedIndentNoMaterialReqApprovState,
+                              builder: (context, item) {
+                                return SearchChoices<IndentNo>.single(
+                                  icon: const Icon(Icons.keyboard_arrow_down_sharp,size:30),
+                                  padding: selectIndentNo!=null ? 2 : 11,
+                                  isExpanded: true,
+                                  hint: "Search here",
+                                  value: selectIndentNo,
+                                  displayClearIcon: false,
+                                  onChanged: onDataChange1,
+                                  items: snapshot?.data
+                                      ?.map<DropdownMenuItem<IndentNo>>((e) {
+                                    return DropdownMenuItem<IndentNo>(
+                                      value: e,
+                                      child: Text(e.StrDocID ?? ''),
+                                    );
+                                  })?.toList() ??[],
+                                );
+                              }
+                          );
+                        }
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10,),
+                StreamBuilder(
+                  stream: getIndentItemData().asStream(),
+      builder: (context, snapshot){
+      if(snapshot.hasData){
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 15.0),
+          child: Container(height: height * 0.35,
+            child: ListView.builder(
+              physics: AlwaysScrollableScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              itemBuilder:(context, index){
+              return Card(
 child: ListTile(
   leading: Text(snapshot.data[index]['Item']),
 ),
-          );
-          }
-        ),
-      );
-//         return Column(
-//           children:<Widget>[
-//               ListView.builder(
-//                 shrinkWrap: true,
-//                  itemExtent: snapshot.data.length,
-//       itemBuilder: (context, index) =>
-//                 SingleChildScrollView( scrollDirection: Axis.vertical,
-//                   child: Card(elevation:5.0,margin:  EdgeInsets.symmetric(vertical: 1,horizontal: 6),
-//   child: ListTile(
-//   leading: Text(snapshot.data[index]['Item']),
-//
-// ),
-//                   ),
-//                 )
-//
-//                ),
-//           ]
-//         );
-    }
-    else{
-      return
-      Center(child: CircularProgressIndicator());}
-    }
-              ),
-              // const InformationBoxContainer1(),
-              //SizedBox(height: 10,),
-              //const InformationBoxContainer1(),
-              // const MaterialApprovalPageContainerData(),
-              sizedbox1,
-              Padding(
-                  padding: padding4,
-                  child: roundedButtonHome2("Approve",(){
-
-                  },roundedButtonHomeColor1)),
-              Padding(
-                  padding: padding4,
-                  child: roundedButtonHome2("Deny",(){},roundedButtonHomeColor2)),
-              Padding(
-                  padding: padding4,
-                  child: roundedButtonHome2("Wait",(){},roundedButtonHomeColor3)),
-            ],
+              );
+              }
+            ),
+          ),
+        );
+      }
+      else{
+        return
+        Center(child: CircularProgressIndicator());}
+      }
+                ),
+                // const InformationBoxContainer1(),
+                //SizedBox(height: 10,),
+                //const InformationBoxContainer1(),
+                // const MaterialApprovalPageContainerData(),
+                sizedbox1,
+                Padding(
+                    padding: padding4,
+                    child: roundedButtonHome2("Approve",(){
+                    },roundedButtonHomeColor1)),
+                Padding(
+                    padding: padding4,
+                    child: roundedButtonHome2("Deny",(){},roundedButtonHomeColor2)),
+              ],
+            ),
           ),
         ),
       ),
