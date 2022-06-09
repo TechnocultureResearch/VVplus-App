b7c6fb23a6834dc7ec14f82c5ca3f7fec907a4d8
krishna40-dot <krish.nand.i40@gmail.com>
2022-06-09 13:44:34 +0530

    feat: added DeptName BLoc for MaterialReqEntry

--- lib/Application/Bloc/Dropdown_Bloc/department_name_dropdown_bloc.dart
+++ lib/Application/Bloc/Dropdown_Bloc/department_name_dropdown_bloc.dart
@@ -6,17 +6,24 @@ import 'dart:async';
 class DepartmentNameDropdownBloc {
   final departmentNameRepository = DepartmentNameRepository();
   final departmentNameGetData = BehaviorSubject<DepartmentName>();
+  final departmentMaterialReqEntryGetData = BehaviorSubject<DepartmentName>();
 
   Future<List<DepartmentName>> departmentNameData;
+  Future<List<DepartmentName>> departmentMaterialReqEntryData;
 
   Stream<DepartmentName> get selectedState => departmentNameGetData;
   void selectedStateEvent(DepartmentName state) => departmentNameGetData.add(state);
 
+  Stream<DepartmentName> get selectedDepartmentMaterialReqEntryState => departmentMaterialReqEntryGetData;
+void selectedDepartmentMaterialReqEntryStateEvent(DepartmentName state) => departmentMaterialReqEntryGetData.add(state);
+
   DepartmentNameDropdownBloc() {
     departmentNameData = departmentNameRepository.getData();
+    departmentMaterialReqEntryData = departmentNameRepository.getDepartmentMaterialReqEntryData();
   }
 
   void dispose() {
     departmentNameGetData.close();
+    departmentMaterialReqEntryGetData.close();
   }
 }
