ff6c0ed8559bb5ec5b37ac1cada14da599d04a4b
krishna40-dot <krish.nand.i40@gmail.com>
2022-06-11 15:30:42 +0530

    ci: added item api for Material Req Entry

--- lib/infrastructure/Repository/item_name_repository.dart
+++ lib/infrastructure/Repository/item_name_repository.dart
@@ -7,34 +7,47 @@ import 'dart:async';
 import 'package:vvplus_app/infrastructure/Models/item_name_model.dart';
 
 
+
 class ItemNameRepository {
   Client client = Client();
 
-  Future<List<ItemName>> getItemStockReceiveEntryData() async {
+  Future<List<ItemNameModel>> getItemStockReceiveEntryData() async {
     try {
       final response = await client.get(Uri.parse(ApiService.getItemNameStockReceiveEntrynewURL));
       final items = (jsonDecode(response.body) as List)
-          .map((e) => ItemName.fromJson(e))
+          .map((e) => ItemNameModel.fromJson(e))
           .toList();
       return items;
     } catch (e) {
       rethrow;
     }
   }
-  Future<List<ItemName>> getStockissueItemData() async {
+  Future<List<ItemNameModel>> getStockissueItemData() async {
     try{
       final response = await client.get(Uri.parse(ApiService.getStockIssueItemCurrentStatusnewURL));
       final items = (jsonDecode(response.body) as List)
-          .map((e) => ItemName.fromJson(e))
+          .map((e) => ItemNameModel.fromJson(e))
           .toList();
       return items;
     } catch (e) {
+      rethrow;
+    }
+  }
+  Future<List<ItemNameModel>> getItemMaterialReqEntryData() async{
+    try{
+final response = await client.get(Uri.parse(ApiService.getItemMaterialReqEntrynewURL));
+final items = (jsonDecode(response.body) as List)
+.map((e) => ItemNameModel.fromJson(e))
+.toList();
+return items;
+    }catch(e){
       rethrow;
     }
   }
 }
 
-Future<List<ItemName>> createUser(String strSubCode,String strName) async{
+
+Future<List<ItemNameModel>> createUser(String strSubCode,String strName) async{
   final response = await http.post(Uri.parse(ApiService.getItemNameURL), body: {
     "StrSubCode": strSubCode,
     "StrName": strName,
