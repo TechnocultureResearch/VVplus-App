cc42788e3a589ac772652df3c5eabf74be690a79
payalrparmar <payalparmar.r1@gmail.com>
2022-06-22 10:58:52 +0530

    perf:add name data in change applicable model

--- lib/infrastructure/Models/change_applicable_model.dart
+++ lib/infrastructure/Models/change_applicable_model.dart
@@ -8,22 +8,22 @@ String changeApplicableToJson(List<ChangeApplicable> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class ChangeApplicable {
-  ChangeApplicable({
-    this.strSubCode,
-    this.strName,
-  });
+  ChangeApplicable({this.strSubCode, this.strName, this.name});
 
   String strSubCode;
   String strName;
+  String name;
 
   factory ChangeApplicable.fromJson(Map<String, dynamic> json) =>
       ChangeApplicable(
         strSubCode: json["StrSubCode"],
         strName: json["StrName"],
+        name: json["Name"],
       );
 
   Map<String, dynamic> toJson() => {
         "StrSubCode": strSubCode,
         "StrName": strName,
+        "Name": name,
       };
 }
